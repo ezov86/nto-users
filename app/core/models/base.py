7cb15a17f118423337b19524771ec178e862912d
mt <a>
2023-03-14 07:24:30 +0000

    models: Base.update_fields() fix

--- app/core/models/base.py
+++ app/core/models/base.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     def update_fields(self, **kwargs):
-        for key, value in kwargs:
+        for key, value in kwargs.items():
             setattr(self, key, value)
